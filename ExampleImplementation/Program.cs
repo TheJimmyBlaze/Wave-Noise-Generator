db91da30e9003591714c253ed818f809c7a1c5cb
Cameron Taylor <TheJimmyBlaze@gmail.com>
2019-03-09 14:07:39 +1300

    Refactored Example
    Refactored example to put the good stuff in a single method, added some comments to explain how the generator should be used.

--- ExampleImplementation/Program.cs
+++ ExampleImplementation/Program.cs
@@ -26,25 +26,8 @@ namespace ExampleImplementation
             if (!regex.IsMatch(path))
                 path += EXTENSION;
 
-            LatticeNoiseGenerator generator = new LatticeNoiseGenerator(typeof(SineWave));
-
-            Random random = new Random((int)DateTime.Now.Ticks);
-            int xOffset = random.Next();
-            int yOffset = random.Next();
-
-            byte[] heightMap = new byte[SIZE * SIZE];
-            for (int x = 0; x < SIZE; x++)
-            {
-                for (int y = 0; y < SIZE; y++)
-                {
-                    heightMap[x + y * SIZE] = (byte)generator.Observe(x + xOffset, y + yOffset, SIZE, 255, 0.25, 0.1);
-                }
-            }
-
-            Bitmap converted = BitmapConverter.Convert(heightMap, SIZE);
-            Bitmap save = new Bitmap(converted);
-            converted.Dispose();
-            save.Save(path);
+            Bitmap map = GenerateHeightMap(); // <-- This is the good stuff!
+            map.Save(path);
 
             Console.WriteLine("Created bitmap: {0}", path);
             Console.WriteLine("Open file? (Y\\N)");
@@ -63,6 +46,35 @@ namespace ExampleImplementation
             Console.WriteLine("Save a sample bitmap by entering a file path:");
 
             return Console.ReadLine();
+        }
+
+        private static Bitmap GenerateHeightMap()
+        {
+            //Instatiate a new LatticeNoiseGenerator passing the type of WaveGenerator to be applied to the lattice.
+            LatticeNoiseGenerator generator = new LatticeNoiseGenerator(typeof(SineWave));
+
+            //Create a random starting x and y point, this is to ensure that we get a random height map everytime we call GenerateHeightMap.
+            Random random = new Random((int)DateTime.Now.Ticks);
+            int xOffset = random.Next();
+            int yOffset = random.Next();
+
+            //Create a byte array to store all of the generated heights.
+            byte[] heightMap = new byte[SIZE * SIZE];
+            for (int x = 0; x < SIZE; x++)
+            {
+                for (int y = 0; y < SIZE; y++)
+                {
+                    //Populate each of the points in the array one at a time, all we need is a point in coordenant space and some input variables.
+                    //I have set the hypothetical size to be the same as the bitmap image we are creating, if you are generating maps without a size constraint, you can really put anything in here.
+                    //The frequency has been set to 0.25, this gives a nice smooth heightmap, if you want it to be noisy, try increasing to 1 or 2. If you want an even smoother map, lower it some more.
+                    //The density has been set to 0.1, this means that the hypotetical size is divided into 10 lattice points each of size 10, and wave forms are attached to these points, and increased density will also increase noisyness.
+                    heightMap[x + y * SIZE] = (byte)generator.Observe(x + xOffset, y + yOffset, SIZE, 255, 0.25, 0.1);
+                }
+            }
+
+            //Return a new bitmap, can't return the raw bitmap, as the converter must be disposed correctly before we can write to file.
+            using (Bitmap raw = BitmapConverter.Convert(heightMap, SIZE))
+                return new Bitmap(raw);
         }
     }
 }
